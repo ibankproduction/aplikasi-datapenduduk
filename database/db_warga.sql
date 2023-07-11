fe09328a67f50af6dd448208213d21d94dc7a589
Ibank Albern <m.iqbal.nasutioon@gmail.com>
2023-07-11 11:55:18 +0700

    first commit

---
+++ database/db_warga.sql
@@ -0,0 +1,305 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 11, 2023 at 06:30 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_warga`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `galeri`
+--
+
+CREATE TABLE `galeri` (
+  `id_galeri` int(11) NOT NULL,
+  `path_galeri` varchar(100) NOT NULL,
+  `caption_galeri` text NOT NULL,
+  `tautan_galeri` varchar(100) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kartu_keluarga`
+--
+
+CREATE TABLE `kartu_keluarga` (
+  `id_keluarga` int(11) NOT NULL,
+  `nomor_keluarga` varchar(16) NOT NULL,
+  `id_kepala_keluarga` int(11) NOT NULL,
+  `alamat_keluarga` text NOT NULL,
+  `desa_kelurahan_keluarga` varchar(30) NOT NULL,
+  `kecamatan_keluarga` varchar(30) NOT NULL,
+  `kabupaten_kota_keluarga` varchar(30) NOT NULL,
+  `provinsi_keluarga` varchar(30) NOT NULL,
+  `negara_keluarga` varchar(30) NOT NULL,
+  `rt_keluarga` varchar(3) NOT NULL,
+  `rw_keluarga` varchar(3) NOT NULL,
+  `kode_pos_keluarga` varchar(5) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kartu_keluarga`
+--
+
+INSERT INTO `kartu_keluarga` (`id_keluarga`, `nomor_keluarga`, `id_kepala_keluarga`, `alamat_keluarga`, `desa_kelurahan_keluarga`, `kecamatan_keluarga`, `kabupaten_kota_keluarga`, `provinsi_keluarga`, `negara_keluarga`, `rt_keluarga`, `rw_keluarga`, `kode_pos_keluarga`, `id_user`, `created_at`, `updated_at`) VALUES
+(1, '12121212121', 1, 'Ciangsana', 'ciangsana', 'Gunung Putri', 'Bogor', 'Jawa Barat', 'Indonesia', '003', '015', '16968', 1, '2023-06-03 10:38:51', '2023-06-03 10:38:51');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mutasi`
+--
+
+CREATE TABLE `mutasi` (
+  `id_mutasi` int(11) NOT NULL,
+  `nik_mutasi` varchar(16) NOT NULL,
+  `nama_mutasi` varchar(45) NOT NULL,
+  `tempat_lahir_mutasi` varchar(30) NOT NULL,
+  `tanggal_lahir_mutasi` date NOT NULL,
+  `jenis_kelamin_mutasi` enum('L','P') NOT NULL,
+  `alamat_ktp_mutasi` text NOT NULL,
+  `alamat_mutasi` text NOT NULL,
+  `desa_kelurahan_mutasi` varchar(30) NOT NULL,
+  `kecamatan_mutasi` varchar(30) NOT NULL,
+  `kabupaten_kota_mutasi` varchar(30) NOT NULL,
+  `provinsi_mutasi` varchar(30) NOT NULL,
+  `negara_mutasi` varchar(30) NOT NULL,
+  `rt_mutasi` varchar(3) NOT NULL,
+  `rw_mutasi` varchar(3) NOT NULL,
+  `agama_mutasi` enum('Islam','Kristen','Katholik','Hindu','Budha','Konghucu') NOT NULL,
+  `pendidikan_terakhir_mutasi` varchar(20) NOT NULL,
+  `pekerjaan_mutasi` varchar(20) NOT NULL,
+  `status_perkawinan_mutasi` enum('Kawin','Tidak Kawin') NOT NULL,
+  `status_mutasi` enum('Tetap','Kontrak') NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `nama_user` varchar(45) NOT NULL,
+  `username_user` varchar(20) NOT NULL,
+  `password_user` varchar(32) NOT NULL,
+  `keterangan_user` text NOT NULL,
+  `status_user` enum('Admin','RT','RW') NOT NULL,
+  `desa_kelurahan_user` varchar(30) NOT NULL,
+  `kecamatan_user` varchar(30) NOT NULL,
+  `kabupaten_kota_user` varchar(30) NOT NULL,
+  `provinsi_user` varchar(30) NOT NULL,
+  `negara_user` varchar(30) NOT NULL,
+  `rt_user` varchar(3) NOT NULL,
+  `rw_user` varchar(3) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `nama_user`, `username_user`, `password_user`, `keterangan_user`, `status_user`, `desa_kelurahan_user`, `kecamatan_user`, `kabupaten_kota_user`, `provinsi_user`, `negara_user`, `rt_user`, `rw_user`, `created_at`, `updated_at`) VALUES
+(1, 'admin', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin di aplikasi pendataan warga', 'Admin', 'kebun jahe', 'binjai', 'medan', 'sumatera utara', 'Indonesia', '001', '002', '2023-06-03 10:29:49', '2017-10-19 06:31:17');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `warga`
+--
+
+CREATE TABLE `warga` (
+  `id_warga` int(11) NOT NULL,
+  `nik_warga` varchar(16) NOT NULL,
+  `nama_warga` varchar(45) NOT NULL,
+  `tempat_lahir_warga` varchar(30) NOT NULL,
+  `tanggal_lahir_warga` date NOT NULL,
+  `jenis_kelamin_warga` enum('L','P') NOT NULL,
+  `alamat_ktp_warga` text NOT NULL,
+  `alamat_warga` text NOT NULL,
+  `desa_kelurahan_warga` varchar(30) NOT NULL,
+  `kecamatan_warga` varchar(30) NOT NULL,
+  `kabupaten_kota_warga` varchar(30) NOT NULL,
+  `provinsi_warga` varchar(30) NOT NULL,
+  `negara_warga` varchar(30) NOT NULL,
+  `rt_warga` varchar(3) NOT NULL,
+  `rw_warga` varchar(3) NOT NULL,
+  `agama_warga` enum('Islam','Kristen','Katholik','Hindu','Budha','Konghucu') NOT NULL,
+  `pendidikan_terakhir_warga` varchar(20) NOT NULL,
+  `pekerjaan_warga` varchar(20) NOT NULL,
+  `status_perkawinan_warga` enum('Kawin','Tidak Kawin') NOT NULL,
+  `status_warga` enum('Tetap','Kontrak') NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `warga`
+--
+
+INSERT INTO `warga` (`id_warga`, `nik_warga`, `nama_warga`, `tempat_lahir_warga`, `tanggal_lahir_warga`, `jenis_kelamin_warga`, `alamat_ktp_warga`, `alamat_warga`, `desa_kelurahan_warga`, `kecamatan_warga`, `kabupaten_kota_warga`, `provinsi_warga`, `negara_warga`, `rt_warga`, `rw_warga`, `agama_warga`, `pendidikan_terakhir_warga`, `pekerjaan_warga`, `status_perkawinan_warga`, `status_warga`, `id_user`, `created_at`, `updated_at`) VALUES
+(1, '2121212121212121', 'sulaiman', 'juluk timur', '1985-02-26', 'L', 'asasasasasas', 'asasasasasasa', 'kebun jahe', 'binjai', 'medan', 'sumatera utara', 'Indonesia', '001', '002', 'Islam', 'SMP', 'wwwwwwww', 'Kawin', 'Kontrak', 1, '2017-10-19 07:25:43', '0000-00-00 00:00:00'),
+(2, '01', 'agus', 'bogor', '2023-06-03', 'L', 'ciangsana', 'ciangsana', 'ciangsana', 'gunung putri', 'bogor', 'jawa barat', 'Indonesia', '003', '015', 'Islam', 'Tidak Tamat SD', 'wirawsasta', 'Kawin', 'Tetap', 1, '2023-06-03 10:53:07', '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `warga_has_kartu_keluarga`
+--
+
+CREATE TABLE `warga_has_kartu_keluarga` (
+  `id_warga` int(11) NOT NULL,
+  `id_keluarga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `galeri`
+--
+ALTER TABLE `galeri`
+  ADD PRIMARY KEY (`id_galeri`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indexes for table `kartu_keluarga`
+--
+ALTER TABLE `kartu_keluarga`
+  ADD PRIMARY KEY (`id_keluarga`),
+  ADD KEY `id_kepala_keluarga` (`id_kepala_keluarga`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indexes for table `mutasi`
+--
+ALTER TABLE `mutasi`
+  ADD PRIMARY KEY (`id_mutasi`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- Indexes for table `warga`
+--
+ALTER TABLE `warga`
+  ADD PRIMARY KEY (`id_warga`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indexes for table `warga_has_kartu_keluarga`
+--
+ALTER TABLE `warga_has_kartu_keluarga`
+  ADD KEY `id_penduduk` (`id_warga`,`id_keluarga`),
+  ADD KEY `warga_has_kartu_keluarga_ibfk_2` (`id_keluarga`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `galeri`
+--
+ALTER TABLE `galeri`
+  MODIFY `id_galeri` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `kartu_keluarga`
+--
+ALTER TABLE `kartu_keluarga`
+  MODIFY `id_keluarga` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `mutasi`
+--
+ALTER TABLE `mutasi`
+  MODIFY `id_mutasi` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `warga`
+--
+ALTER TABLE `warga`
+  MODIFY `id_warga` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `galeri`
+--
+ALTER TABLE `galeri`
+  ADD CONSTRAINT `galeri_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+
+--
+-- Constraints for table `kartu_keluarga`
+--
+ALTER TABLE `kartu_keluarga`
+  ADD CONSTRAINT `kartu_keluarga_ibfk_1` FOREIGN KEY (`id_kepala_keluarga`) REFERENCES `warga` (`id_warga`),
+  ADD CONSTRAINT `kartu_keluarga_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+
+--
+-- Constraints for table `mutasi`
+--
+ALTER TABLE `mutasi`
+  ADD CONSTRAINT `mutasi_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+
+--
+-- Constraints for table `warga`
+--
+ALTER TABLE `warga`
+  ADD CONSTRAINT `warga_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+
+--
+-- Constraints for table `warga_has_kartu_keluarga`
+--
+ALTER TABLE `warga_has_kartu_keluarga`
+  ADD CONSTRAINT `warga_has_kartu_keluarga_ibfk_1` FOREIGN KEY (`id_warga`) REFERENCES `warga` (`id_warga`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `warga_has_kartu_keluarga_ibfk_2` FOREIGN KEY (`id_keluarga`) REFERENCES `kartu_keluarga` (`id_keluarga`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
